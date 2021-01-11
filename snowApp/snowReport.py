3e55d04741c4e64647f45dc160237d5e9b5001b6
Steven <stevenhcau@gmail.com>
2021-01-10 19:33:39 -0700

    Updated snowReport.py to reduce the size of the __init__ file and break up the rest of the functionality into class methods. This is so that unit testing may more easily be performed

--- snowApp/snowReport.py
+++ snowApp/snowReport.py
@@ -88,11 +88,18 @@ def addNewResort(resortKey, resortName, country, lat, lon, *args):
 # Get request modified to only pull the data requested by the user using args
 # Question: are kwargs or args better to use in this situation?
 # Defines a class "Resort" to handle the attributes and methods for each ski resort
+# Init function creates a new instance of your class
+# Init function should only initiate your variable
+# Lines 105 to 108, good to instantiate in the init
+# self.args = args
+# create a new function that is called process request
+# request item in self.args
 class Resort():
     # kwargs is created so the user can pass in "96hr", "realtime", and or "360min"
     def __init__(self, resortKey, *args):
         # Checks if the user enters arguments to initiate json files or not
         self.dataJSON = SKI_RESORT_JSON
+        self.args = args
 
         if not args:
             raise Exception("Invalid arg passed. Function arguments must contain one of '360min' and/or '96hr' and/or 'realtime'") # Note there is a hidden arg that is called test that does not access the api to do a mock test
@@ -107,87 +114,95 @@ class Resort():
         self.lat = resortDict["lat"]
         self.country = resortDict["country"]
 
-        # user needs to enter the request required
-        # "all" means user is requesting json for all 3 weather types
-        # "realtime" = realtime
-        # "360min" = 360min
-        # "96hr" = 96hr
-        for requestItem in args:
+        self.weatherJsonRealTime = {}
+        self.weatherJson360Min = {}
+        self.weatherJson96hr = {}
+
+
+
+    def processRequests(self):
+        for requestItem in self.args:
+            # Makes a request for realtime data
             if requestItem == "realtime":
-                querystring = {
-                    "lat": str(resortDict["lat"]),
-                    "lon": str(resortDict["lon"]),
-                    "unit_system": "si",
-                    "fields": "precipitation,precipitation_type,temp,feels_like,wind_speed,wind_direction,sunrise,sunset,visibility,cloud_cover,cloud_base,weather_code",
-                    "apikey": CLIMACELL_KEY,
-                }
-
-                response = requests.request("GET", URL_REALTIME, params=querystring)
-                self.weatherJsonRealTime = json.loads(response.text)
-
-                self.nowTime = localTime(self.weatherJsonRealTime["observation_time"]["value"])
-                self.nowTemp = self.weatherJsonRealTime["temp"]["value"]
-                self.nowFeelsLike = self.weatherJsonRealTime["feels_like"]["value"]
-                self.nowPrecipitation = self.weatherJsonRealTime["precipitation"]["value"]
-                self.nowPrecipitationType = self.weatherJsonRealTime["precipitation_type"]["value"]
-                self.nowWindSpeed = self.weatherJsonRealTime["wind_speed"]["value"]
-                self.nowWindDirection = self.weatherJsonRealTime["wind_direction"]["value"]
-                self.nowCloudCover = self.weatherJsonRealTime["cloud_cover"]["value"]
+                self.requestRealtime()
 
             # Makes a request for 360min forecast and stores in self.weatherJson360Min
             elif requestItem == "360min":
-                querystring = {
-                    "lat": str(resortDict["lat"]),
-                    "lon": str(resortDict["lon"]),
-                    "unit_system": "si",
-                    "timestep": "5",
-                    "start_time": "now",
-                    "fields": "temp,feels_like,humidity,wind_speed,wind_direction,precipitation,precipitation_type,sunrise,sunset,visibility,cloud_cover,cloud_base,weather_code",
-                    "apikey": CLIMACELL_KEY,
-                }
+                self.request360min()
 
-                response = requests.request("GET", URL_NOWCAST, params=querystring)
-                self.weatherJson360Min = json.loads(response.text)
-            
             # Makes a request for 4 day (96hr) forecast and stores in self.weatherJson96hr
             elif requestItem == "96hr":
-                querystring = {
-                    "lat": str(self.lat),
-                    "lon": str(self.lon),
-                    "unit_system": "si",
-                    "start_time": "now",
-                    "fields": "precipitation,temp,feels_like,humidity,wind_speed,wind_direction,precipitation_type,precipitation_probability,sunrise,sunset,cloud_cover,cloud_base,weather_code",
-                    "apikey": CLIMACELL_KEY,
-                }
-                response = requests.request("GET", URL_HOURLY, params=querystring)  # ClimaCell: The hourly call provides a global hourly forecast, up to 96 hours (4 days) out, for a specific location.
-                self.weatherJson96hr = json.loads(response.text)
-                
-                # FIXED: This is causing an error - if there is no snow then in returns an empty list
-                # I fixed this error by adding an else assigner to the list comprehension, if there is no snow in the forecast then it assigns an int value of 0 to the key in the list
-                # The methods totalSnow96hr and checkSnow96hr have been changed so that they reflect that even if the list is not empty, that does not necessarily mean there is snow in the forecast
-                # Previously those methods checked if the list was longer than a length of 0, if true, then snow should exist
-                self.snowForecast96hr = [
-                    (
-                     self.weatherJson96hr[i]["precipitation"]["value"]
-                    if self.weatherJson96hr[i]["precipitation_type"]["value"] == "snow"
-                    else 0   
-                    )
-                    for i in range(0, len(self.weatherJson96hr))
-                ]
+                self.request96hr()
 
-                # Calculates the amount of snow in the next 96hr
-                self.accumulatedSnow = 0
-                for snow in self.snowForecast96hr:
-                    self.accumulatedSnow = float(snow) + self.accumulatedSnow
-                
-                # Creates an attribute called self.isSnow to indicate to the user if there is snow or not
-                if self.accumulatedSnow > 0:
-                    self.isSnow = True
-                else:
-                    self.isSnow = False
+    # Requests realtime data
+    def requestRealtime(self):
+        querystring = {
+            "lat": str(self.lat),
+            "lon": str(self.lon),
+            "unit_system": "si",
+            "fields": "precipitation,precipitation_type,temp,feels_like,wind_speed,wind_direction,sunrise,sunset,visibility,cloud_cover,cloud_base,weather_code",
+            "apikey": CLIMACELL_KEY,
+        }
+
+        response = requests.request("GET", URL_REALTIME, params=querystring)
+        self.weatherJsonRealTime = json.loads(response.text)
+
+        self.nowTime = localTime(self.weatherJsonRealTime["observation_time"]["value"])
+        self.nowTemp = self.weatherJsonRealTime["temp"]["value"]
+        self.nowFeelsLike = self.weatherJsonRealTime["feels_like"]["value"]
+        self.nowPrecipitation = self.weatherJsonRealTime["precipitation"]["value"]
+        self.nowPrecipitationType = self.weatherJsonRealTime["precipitation_type"]["value"]
+        self.nowWindSpeed = self.weatherJsonRealTime["wind_speed"]["value"]
+        self.nowWindDirection = self.weatherJsonRealTime["wind_direction"]["value"]
+        self.nowCloudCover = self.weatherJsonRealTime["cloud_cover"]["value"]
+
+        return self.weatherJsonRealTime   
+
+    def request360min(self):
+        querystring = {
+            "lat": str(self.lat),
+            "lon": str(self.lon),
+            "unit_system": "si",
+            "timestep": "5",
+            "start_time": "now",
+            "fields": "temp,feels_like,humidity,wind_speed,wind_direction,precipitation,precipitation_type,sunrise,sunset,visibility,cloud_cover,cloud_base,weather_code",
+            "apikey": CLIMACELL_KEY,
+        }
+        response = requests.request("GET", URL_NOWCAST, params=querystring)
+        self.weatherJson360Min = json.loads(response.text)
+        return self.weatherJson360Min
+
+    def request96hr(self):
+        querystring = {
+            "lat": str(self.lat),
+            "lon": str(self.lon),
+            "unit_system": "si",
+            "start_time": "now",
+            "fields": "precipitation,temp,feels_like,humidity,wind_speed,wind_direction,precipitation_type,precipitation_probability,sunrise,sunset,cloud_cover,cloud_base,weather_code",
+            "apikey": CLIMACELL_KEY,
+        }
+        response = requests.request("GET", URL_HOURLY, params=querystring)  # ClimaCell: The hourly call provides a global hourly forecast, up to 96 hours (4 days) out, for a specific location.
+        self.weatherJson96hr = json.loads(response.text)
+        return self.weatherJson96hr
+
+     
+    def checkSnow(self):
+
+        self.snowIn96hrForecast = [(self.weatherJson96hr[i]["precipitation"]["value"] if self.weatherJson96hr[i]["precipitation_type"]["value"] == "snow" else 0) for i in range(0, len(self.weatherJson96hr))]
+        # Calculates the amount of snow in the next 96hr
+        self.accumulatedSnow = 0
+        for snow in self.snowIn96hrForecast:
+            self.accumulatedSnow = float(snow) + self.accumulatedSnow
+        
+        # Creates an attribute called self.isSnow to indicate to the user if there is snow or not
+        if self.accumulatedSnow > 0:
+            self.isSnow = True
+        else:
+            self.isSnow = False        
 
     # This method  checks if there is snow in the next 4 days and prints it to the user
     def print4DaySnow(self):
+        self.checkSnow()
         if self.isSnow:
             print(self.name + ": SNOW IN THE NEXT 4 DAYS")
             print(f"{self.accumulatedSnow :.2f} mm of snow")
@@ -233,78 +248,5 @@ def queryByCountry(country):
                 resortObj = Resort(key)
                 resortObj.print4DaySnow()
 
-# TODO: Create try and except for the arguments
-# TODO: Create a function and pass it a list of resorts to execute
-
-# resortTest = Resort("sunshine", "96hr")
-# resortTest.print4DaySnow()
-
-# # def main():
-# #     try:
-# #         usage = sys.argv[1].lower()
-# #     except:
-# #         print("Not run from command line")
-# #         print("Please enter usage scenario") # print out the different usage scenario options
-# #         usage = input().lower()
-# #         print('')
-# #     try:
-# #         resortFilter = sys.argv[2].lower() # location filter: favorites, alberta, canada, usa
-# #     except:
-# #         print("Not run from command line")
-# #         print("Please enter filter request") # print out the different filter request
-# #         resortFilter = input().lower()
-# #         print('')
-
-
-    # Add threads to optimize the request option
-
-# #     if resortFilter.lower() == "favorites":
-# #         if usage == "realtime":
-# #             for resort in favorites:
-# #                 resortObj = Resort(resort)
-# #                 resortObj.printRealTimeWeather
-# #         elif usage == "4day":
-
-# #             for resort in favorites:
-# #                 resortObj = Resort(resort)
-# #                 resortObj.print4DaySnow
-# #         elif usage == "96hr":
-# #             for resort in favorites:
-# #                 resortObj = Resort(resort)
-# #                 resortObj.print4DaySnow
-# #         elif usage == "360min":
-# #             for resort in favorites:
-# #                 resortObj = Resort(resort)
-# #                 resortObj.plotTemp()
-# #                 resortObj.plotPrecipitation()
-
-# #     elif resortFilter.lower() == "alberta":
-# #         if usage == "realtime":
-# #             for resort in albertaResorts:
-# #                 resortObj = Resort(resort)
-# #                 resortObj.printRealTimeWeather
-# #         elif usage == "4day":
-# #             for resort in albertaResorts:
-# #                 resortObj = Resort(resort)
-# #                 resortObj.print4DaySnow
-# #         elif usage == "96hr":
-# #             for resort in albertaResorts:
-# #                 resortObj = Resort(resort)
-# #                 resortObj.print4DaySnow
-# #         elif usage == "360min":
-# #             for resort in albertaResorts:
-# #                 resortObj = Resort(resort)
-# #                 resortObj.plotTemp()
-# #     elif usage == "canada":
-# #         queryByCountry("Canada")
-
-# #     elif usage == "usa":
-# #         queryByCountry("USA")
-
-# #     else:
-# #         print("")
-
-# # if __name__ == '__main__':
-# #     main()
-
-# 
+resort = Resort("sunshine", "96hr")
+resort.print4DaySnow()
